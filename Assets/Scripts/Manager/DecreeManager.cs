e6c03083741b80e0cb3cb5458ae913fd15166039
nguyendinhhung04 <dinhhung5a6@gmail.com>
2026-01-31 18:38:50 +0700

    push at 6:38 PM

--- Assets/Scripts/Manager/DecreeManager.cs
+++ Assets/Scripts/Manager/DecreeManager.cs
@@ -10,11 +10,13 @@ public class DecreeManager : MonoBehaviour
     public List<DecreeSO> day0Fixed;
     public List<DecreeSO> day1Fixed;
     public List<DecreeSO> day2Fixed;
+    public List<DecreeSO> day3Fixed; // Added for day 3
 
     [Header("Random Decrees Per Day")]
     public List<DecreeSO> day0Random;
     public List<DecreeSO> day1Random;
     public List<DecreeSO> day2Random;
+    public List<DecreeSO> day3Random; // Added for day 3
 
     private Queue<DecreeSO> todayQueue = new Queue<DecreeSO>();
 
@@ -69,7 +71,8 @@ public class DecreeManager : MonoBehaviour
             case 0: return day0Fixed;
             case 1: return day1Fixed;
             case 2: return day2Fixed;
-            default: return day2Fixed;
+            case 3: return day3Fixed; // Added for day 3
+            default: return day3Fixed; // Default to day3Fixed for days >= 3
         }
     }
 
@@ -80,7 +83,8 @@ public class DecreeManager : MonoBehaviour
             case 0: return day0Random;
             case 1: return day1Random;
             case 2: return day2Random;
-            default: return day2Random;
+            case 3: return day3Random; // Added for day 3
+            default: return day3Random; // Default to day3Random for days >= 3
         }
     }
 
